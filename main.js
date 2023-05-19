ff8eb6aeeade1cc160182862426d472bc7f7db0b
arifkareem <87105055+arifkareem@users.noreply.github.com>
2023-05-19 09:21:00 +0800

    initial push

---
+++ main.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron');
+
+const url = require('url');
+
+const path = require('path');
+
+function createMainWindow(){
+
+    const mainWindow = new BrowserWindow({
+
+        title:'test',
+        width: 1000,
+        height: 600
+
+    })
+
+    const starturl = url.format({
+
+        pathname: path.join(__dirname, 'index.html'),
+
+        protocol:'file',
+    });
+
+    mainWindow.loadURL(starturl);
+ 
+}
+
+app.whenReady().then(createMainWindow);
